a37f92c847886386b2de00a294802fdee8561f16
Brice Person <brice@ideeslibres.org>
2021-05-20 13:21:39 +0200

    Update ign-bookmarklet.js
    Adding :
    - Not opérator
    - Clearing search field
    - Cosmetics

--- ign-bookmarklet.js
+++ ign-bookmarklet.js
@@ -1,5 +1,6 @@
-$('.lastUpdate').after('<div><div><input type="text" id="values" placeholder="bd topo gpkg 2021" /><input type="submit" value="rechercher" onclick="javascript:filterFileNames();" /> <a href="javascript:filterFileNames(true);">liste de liens</a></div><div id="results"></div></div>')
+$('.lastUpdate').after('<div><div><input type="text" id="values" placeholder="bd topo gpkg 2021" style="height:35px" /><a href="javascript:clear()" style="padding: 7px;border: 1px solid #bababa;border-left: none;font-weight: bold;color: #949494;border-radius: 3px;">X</a> <input type="submit" value="rechercher" onclick="javascript:filterFileNames();" style="height:35px" /> <a href="javascript:filterFileNames(true);">liste de liens</a></div><div id="results"></div></div>');
 const fname = /([^\/]{1,})$/;
+$('#values').focus();
 
 function filterFileNames(asText = false) {
   $('#results').html('');
@@ -8,26 +9,37 @@ function filterFileNames(asText = false) {
   var v = $('#values').val().trim().toUpperCase().split(' ');
   var results = $("a[href*='7z']");
   for (var i in v) {
-    results = results.filter("a[href*='"+v[i]+"']")
+    if (v[i].startsWith('-')) {
+      results = results.not("a[href*='"+v[i].substring(1)+"']")
+    }
+    else {
+      results = results.filter("a[href*='"+v[i]+"']")
+    }
   }
   results.each(function() {
     arr.push($(this).attr("href"));
   });
   if (arr.length > 0) {
+    var r = 'résultats';
+    if (arr.length === 1) {
+      r = 'résultat';
+    }
     if (asText) {
       for (var i in arr.sort()) {
         text += arr[i]+"\n";
       }
-      $('#results').html('<p>'+arr.length+' résultats</p><textarea style="width:100%;height:200px;">'+text+'</textarea>');
+      $('#results').html('<p>'+arr.length+' '+r+' :</p><textarea style="width:100%;height:200px;">'+text+'</textarea>');
     }
     else {
-      text += '<p>'+arr.length+' résultat(s)</p>';
       for (var i in arr.sort()) {
         var match = arr[i].match(fname);
         text += '<a href="'+arr[i]+'">'+match[1]+'</a><br />'+"\n";
       }
-      $('#results').html(text);
+      $('#results').html('<p>'+arr.length+' '+r+' :</p>'+"\n"+text);
     }
+  }
+  else {
+    $('#results').html('<p>Aucun résultat</p>');
   }
 }
 
@@ -36,3 +48,8 @@ $("#values").keypress(function(event) {
     filterFileNames();
   }
 });
+
+function clear() {
+  $('#values').val('');
+  $('#values').focus();
+}
