6e807002270986bb4965a0d861e5635226923928
Brice Person <brice@ideeslibres.org>
2021-05-21 17:27:38 +0200

    Add documentation links with filtering

--- ign-bookmarklet.js
+++ ign-bookmarklet.js
@@ -1,20 +1,36 @@
-$('.lastUpdate').after('<div><div><input type="text" id="values" placeholder="bd topo gpkg 2021" style="height:35px" title="Pour exclure un terme, préfixez le avec le signe moins. Ex: -shp" /><a href="javascript:clear()" style="padding: 7px;border: 1px solid #bababa;border-left: none;font-weight: bold;color: #949494;border-radius: 3px;">X</a> <input type="submit" value="rechercher" onclick="javascript:filterFileNames();" style="height:35px" /> <a href="javascript:filterFileNames(true);">liste de liens</a></div><div id="results"></div></div>');
+$('.lastUpdate').after('<div><div><input type="text" id="values" placeholder="bd topo gpkg 2021" style="height:35px" title="Pour exclure un terme, préfixez le avec le signe moins. Ex: -shp" /><a href="javascript:clear()" style="padding: 7px;border: 1px solid #bababa;border-left: none;font-weight: bold;color: #949494;border-radius: 3px;">X</a> <input type="submit" value="rechercher" onclick="javascript:filterFileNames();" style="height:35px" /> <a href="javascript:filterFileNames(true);">liste de liens</a></div><div id="docs"></div><div id="results"></div></div>');
+
+const docus = {"admin_express": "ADMIN_EXPRESS", "admin_express_cog": "ADMIN_EXPRESS_COG", "adresse_premium": "ADRESSE_PREMIUM", "bdcarthage": "BDCARTHAGE", "bdcarto": "BDCARTO", "bdcarto_em": "BDCARTO_EM", "bdforet": "BDFORET", "bdpr": "BDPR", "bdtopo": "BDTOPO", "bdtopo_postgresql": "BDTOPO_PostGreSQL", "contours_iris": "CONTOURS_IRIS", "dl_commun": "DL_Commun", "donnees_geodesiques": "DONNEES_GEODESIQUES", "ebm": "EBM", "egm": "EGM", "erm": "ERM", "geofla": "GEOFLA", "iris_ge": "IRIS_GE", "ocs_ge": "OCS_GE", "route120": "ROUTE120", "route500": "ROUTE500", "rpg": "RPG", "suivi-des-bocages": "SUIVI DES BOCAGES", "dl_commun-1": "DL_Commun", "plan-ign": "PLAN IGN", "scan_oaci": "SCAN_OACI", "scan25": "SCAN25", "scan50_histo": "SCAN50_HISTO", "scan100": "SCAN100", "scan500": "SCAN500", "scan1000": "SCAN1000", "scanem10k": "SCANEM10K", "scanem40k": "SCANEM40K", "scanreg": "SCANREG", "applications": "APPLICATIONS", "ressources": "RESSOURCES", "bdaltiv2": "BDALTIV2", "histolitt": "HISTOLITT", "litto3d": "LITTO3D", "ref3dnat": "REF3DNAT", "rge_alti": "RGE_ALTI", "territoire3d": "TERRITOIRE3D", "bdortho_orthohr": "BDORTHO_ORTHOHR", "bdorthohisto": "BDORTHOHISTO", "dl_commun-2": "DL_Commun", "ortho-sat": "ORTHO-SAT", "bdparcellaire": "BDPARCELLAIRE", "parcellaire_express_pci": "Parcellaire_Express_PCI", "comparatif_vegetation": "COMPARATIF_VEGETATION", "jpeg2000": "JPEG2000", "metadonnees": "METADONNEES", "projets_carto": "PROJETS_CARTO"};
+
+var links = '<p>Documentation :</p>';
+for (var d in docus) {
+  links += '<a href="https://geoservices.ign.fr/documentation/diffusion/documentation-offre.html#'+d+'" target="_blank">&#10037; '+docus[d]+'</a>'+"\n";
+}
+$('#docs').html('<p>'+links+'</p>');
+$('#docs a').css({"border": "1px solid #bababa", "border-radius": "4px", "font-size": "0.7em", "padding": "2px 5px", "margin": "0 5px 8px 0", "display": "inline-block"});
+
+const documentation = $("#docs a");
 const fname = /([^\/]{1,})$/;
 const downloadable = $.merge($("a[href*='7z']"), $("a[href*='zip']"));
+
 $('#values').focus();
 
 function filterFileNames(asText = false) {
   $('#results').html('');
+  $('#docs a').css('display', 'none');
   arr = [];
   var text = '';
   var v = $('#values').val().trim().toUpperCase().split(' ');
   var results = downloadable;
+  var docs = documentation;
   for (var i in v) {
     if (v[i].startsWith('-')) {
       results = results.not("a[href*='"+v[i].substring(1)+"']")
+      $("#docs a:contains('"+v[i].substring(1)+"')").css('display', 'none');
     }
     else {
-      results = results.filter("a[href*='"+v[i]+"']")
+      results = results.filter("a[href*='"+v[i]+"']");
+      $("#docs a:contains('"+v[i]+"')").css('display', 'inline-block');
     }
   }
   results.each(function() {
