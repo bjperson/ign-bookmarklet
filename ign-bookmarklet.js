2147f3228d46e6899de8c5fb1b4bb42f6929c354
Brice Person <brice@ideeslibres.org>
2021-05-23 06:21:56 +0200

    Add alternate download

--- ign-bookmarklet.js
+++ ign-bookmarklet.js
@@ -1,4 +1,4 @@
-$('.lastUpdate').after('<div><div><input type="text" id="values" placeholder="bd topo gpkg 2021" style="height:35px" title="Pour exclure un terme, préfixez le avec le signe moins. Ex: -shp" /><a href="javascript:clear()" style="padding: 7px;border: 1px solid #bababa;border-left: none;font-weight: bold;color: #949494;border-radius: 3px;">X</a> <input type="submit" value="rechercher" onclick="javascript:filterFileNames();" style="height:35px" /> <a href="javascript:filterFileNames(true);">liste de liens</a></div><div id="docs"></div><div id="results"></div></div>');
+$('.lastUpdate').after('<div><div><input type="text" id="values" placeholder="bd topo gpkg 2021" style="height:35px" title="Pour exclure un terme, préfixez le avec le signe moins. Ex: -shp" /><a href="javascript:clear()" style="padding: 7px;border: 1px solid #bababa;border-left: none;font-weight: bold;color: #949494;border-radius: 3px;">X</a> <input type="submit" value="rechercher" onclick="javascript:filterFileNames();" style="height:35px" /> <a href="javascript:filterFileNames(true);">liste de liens</a><p><input type="checkbox" id="alt" name="alt" /><label for="alt" style="font-size:small;" title="lorsque disponible"> Privilégier opendatarchives.fr</label></p></div><div id="docs"></div><div id="results"></div></div>');
 
 const docus = {"admin_express": "ADMIN_EXPRESS", "admin_express_cog": "ADMIN_EXPRESS_COG", "adresse_premium": "ADRESSE_PREMIUM", "bdcarthage": "BDCARTHAGE", "bdcarto": "BDCARTO", "bdcarto_em": "BDCARTO_EM", "bdforet": "BDFORET", "bdpr": "BDPR", "bdtopo": "BDTOPO", "bdtopo_postgresql": "BDTOPO_PostGreSQL", "contours_iris": "CONTOURS_IRIS", "dl_commun": "DL_Commun", "donnees_geodesiques": "DONNEES_GEODESIQUES", "ebm": "EBM", "egm": "EGM", "erm": "ERM", "geofla": "GEOFLA", "iris_ge": "IRIS_GE", "ocs_ge": "OCS_GE", "route120": "ROUTE120", "route500": "ROUTE500", "rpg": "RPG", "suivi-des-bocages": "SUIVI DES BOCAGES", "dl_commun-1": "DL_Commun", "plan-ign": "PLAN IGN", "scan_oaci": "SCAN_OACI", "scan25": "SCAN25", "scan50_histo": "SCAN50_HISTO", "scan100": "SCAN100", "scan500": "SCAN500", "scan1000": "SCAN1000", "scanem10k": "SCANEM10K", "scanem40k": "SCANEM40K", "scanreg": "SCANREG", "applications": "APPLICATIONS", "ressources": "RESSOURCES", "bdaltiv2": "BDALTIV2", "histolitt": "HISTOLITT", "litto3d": "LITTO3D", "ref3dnat": "REF3DNAT", "rge_alti": "RGE_ALTI", "territoire3d": "TERRITOIRE3D", "bdortho_orthohr": "BDORTHO_ORTHOHR", "bdorthohisto": "BDORTHOHISTO", "dl_commun-2": "DL_Commun", "ortho-sat": "ORTHO-SAT", "bdparcellaire": "BDPARCELLAIRE", "parcellaire_express_pci": "Parcellaire_Express_PCI", "comparatif_vegetation": "COMPARATIF_VEGETATION", "jpeg2000": "JPEG2000", "metadonnees": "METADONNEES", "projets_carto": "PROJETS_CARTO"};
 
@@ -6,11 +6,12 @@ var links = '<p>Documentation :</p>';
 for (var d in docus) {
   links += '<a href="https://geoservices.ign.fr/documentation/diffusion/documentation-offre.html#'+d+'" target="_blank">&#10037; '+docus[d]+'</a>'+"\n";
 }
-$('#docs').html('<p>'+links+'</p>');
+$('#docs').html(links);
 $('#docs a').css({"border": "1px solid #bababa", "border-radius": "4px", "font-size": "0.7em", "padding": "2px 5px", "margin": "0 5px 8px 0", "display": "inline-block"});
 
 const documentation = $("#docs a");
 const fname = /([^\/]{1,})$/;
+const altUrl = "https://data.cquest.org";
 const downloadable = $.merge($("a[href*='7z']"), $("a[href*='zip']"));
 
 $('#values').focus();
@@ -18,6 +19,7 @@ $('#values').focus();
 function filterFileNames(asText = false) {
   $('#results').html('');
   $('#docs a').css('display', 'none');
+  alt = $('#alt').prop("checked");
   arr = [];
   var text = '';
   var v = $('#values').val().trim().toUpperCase().split(' ');
@@ -43,14 +45,35 @@ function filterFileNames(asText = false) {
     }
     if (asText) {
       for (var i in arr.sort()) {
-        text += arr[i]+"\n";
+        if (alt) {
+          var match = arr[i].match(fname);
+          if (match[1] in opendatarchives) {
+            text += altUrl+opendatarchives[match[1]]+match[1]+"\n";
+          }
+          else {
+            text += arr[i]+"\n";
+          }
+        }
+        else {
+          text += arr[i]+"\n";
+        }
       }
       $('#results').html('<p>'+arr.length+' '+r+' :</p><textarea style="width:100%;height:200px;">'+text+'</textarea>');
     }
     else {
       for (var i in arr.sort()) {
         var match = arr[i].match(fname);
-        text += '<a href="'+arr[i]+'">'+match[1]+'</a><br />'+"\n";
+        if (alt) {
+          if (match[1] in opendatarchives) {
+            text += '<a href="'+altUrl+opendatarchives[match[1]]+match[1]+'">'+match[1]+'</a><br />'+"\n";
+          }
+          else {
+            text += '<a href="'+arr[i]+'">'+match[1]+'</a><br />'+"\n";
+          }
+        }
+        else {
+          text += '<a href="'+arr[i]+'">'+match[1]+'</a><br />'+"\n";
+        }
       }
       $('#results').html('<p>'+arr.length+' '+r+' :</p>'+"\n"+text);
     }
@@ -60,13 +83,15 @@ function filterFileNames(asText = false) {
   }
 }
 
+function clear() {
+  $('#values').val('');
+  $('#values').focus();
+}
+
 $("#values").keypress(function(event) {
   if (event.keyCode === 13) {
     filterFileNames();
   }
 });
 
-function clear() {
-  $('#values').val('');
-  $('#values').focus();
-}
+$.getScript("https://bjperson.github.io/ign-bookmarklet/resources/extract.js");
