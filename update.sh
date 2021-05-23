df32a5d098abdfb9f0094f1fa13b2aa1e4f20b3d
bjperson <brice@ideeslibres.org>
2021-05-23 04:51:25 +0200

    Add update script

---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+curl https://files.opendatarchives.fr/professionnels.ign.fr/tree.json -o ./resources/opendatarchives2.json
+process=false
+
+if [ -f "./resources/opendatarchives.json" ]
+then
+  old_file=$(md5sum ./resources/opendatarchives.json | sed 's|  [^ ]$||');
+  new_file=$(md5sum ./resources/opendatarchives2.json | sed 's|  [^ ]$||');
+  if [ "$old_file" != "$new_file" ]
+  then
+    rm ./resources/opendatarchives.json
+    mv ./resources/opendatarchives2.json ./resources/opendatarchives.json
+    process=true
+  fi
+else
+  mv ./resources/opendatarchives2.json ./resources/opendatarchives.json
+  process=true
+fi
+
+
+if [ "$process" = true ]
+then
+  echo "opendatarchives = {" > ./resources/extract.js
+  extracted=$(cat ./resources/opendatarchives.json | grep name | sed 's| ||g' | sed 's|^"name.\{1,\}org||' | sort | uniq | grep "7z\|zip" | sed 's|\([^/]\{1,\}\)$| \1|' | sed 's|\([^ ]\{1,\}\) \([^"]\{1,\}\)|"\2": "\1|');
+  echo "${extracted::-1}" >> ./resources/extract.js
+  echo "}" >> ./resources/extract.js
+fi
+
+git config user.name bjperson
+git config user.email bjperson@github.com
+git add ./resources/opendatarchives.json
+git add ./resources/extract.js
+git commit -m "generated extract"
+git push
+
+
