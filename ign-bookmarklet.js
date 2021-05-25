9ca2284143bc32e8c82810bbd37dde5faa95af72
Brice Person <brice@ideeslibres.org>
2021-05-26 00:45:54 +0200

    Clean + add filesize when matched

--- ign-bookmarklet.js
+++ ign-bookmarklet.js
@@ -1,4 +1,4 @@
-$('.lastUpdate').after('<div><div><input type="text" id="values" placeholder="bd topo gpkg 2021" style="height:35px" title="Pour exclure un terme, préfixez le avec le signe moins. Ex: -shp" /><a href="javascript:clear()" style="padding: 7px;border: 1px solid #bababa;border-left: none;font-weight: bold;color: #949494;border-radius: 3px;">X</a> <input type="submit" value="rechercher" onclick="javascript:filterFileNames();" style="height:35px" /> <a href="javascript:filterFileNames(true);">liste de liens</a><p><input type="checkbox" id="alt" name="alt" /><label for="alt" style="font-size:small;" title="lorsque disponible"> Privilégier opendatarchives.fr</label></p></div><div id="docs"></div><div id="results"></div></div>');
+$('.lastUpdate').after('<div><div><input type="text" id="values" placeholder="bd topo gpkg 2021" style="height:35px" title="Pour exclure un terme, préfixez le avec le signe moins. Ex: -shp" /><a href="javascript:clear()" style="padding: 7px;border: 1px solid #bababa;border-left: none;font-weight: bold;color: #949494;border-radius: 3px;">X</a> <input type="submit" value="rechercher" onclick="javascript:filterFileNames();" style="height:35px" /> <a href="javascript:filterFileNames(true);">liste de liens</a><p><input type="checkbox" id="alt" name="alt" /><label for="alt" style="font-size:small;" title="lorsque disponible"> Proposer des liens opendatarchives.fr</label></p></div><div id="docs"></div><div id="results"></div></div>');
 
 const docus = {"admin_express": "ADMIN_EXPRESS", "admin_express_cog": "ADMIN_EXPRESS_COG", "adresse_premium": "ADRESSE_PREMIUM", "bdcarthage": "BDCARTHAGE", "bdcarto": "BDCARTO", "bdcarto_em": "BDCARTO_EM", "bdforet": "BDFORET", "bdpr": "BDPR", "bdtopo": "BDTOPO", "bdtopo_postgresql": "BDTOPO_PostGreSQL", "contours_iris": "CONTOURS_IRIS", "dl_commun": "DL_Commun", "donnees_geodesiques": "DONNEES_GEODESIQUES", "ebm": "EBM", "egm": "EGM", "erm": "ERM", "geofla": "GEOFLA", "iris_ge": "IRIS_GE", "ocs_ge": "OCS_GE", "route120": "ROUTE120", "route500": "ROUTE500", "rpg": "RPG", "suivi-des-bocages": "SUIVI DES BOCAGES", "dl_commun-1": "DL_Commun", "plan-ign": "PLAN IGN", "scan_oaci": "SCAN_OACI", "scan25": "SCAN25", "scan50_histo": "SCAN50_HISTO", "scan100": "SCAN100", "scan500": "SCAN500", "scan1000": "SCAN1000", "scanem10k": "SCANEM10K", "scanem40k": "SCANEM40K", "scanreg": "SCANREG", "applications": "APPLICATIONS", "ressources": "RESSOURCES", "bdaltiv2": "BDALTIV2", "histolitt": "HISTOLITT", "litto3d": "LITTO3D", "ref3dnat": "REF3DNAT", "rge_alti": "RGE_ALTI", "territoire3d": "TERRITOIRE3D", "bdortho_orthohr": "BDORTHO_ORTHOHR", "bdorthohisto": "BDORTHOHISTO", "dl_commun-2": "DL_Commun", "ortho-sat": "ORTHO-SAT", "bdparcellaire": "BDPARCELLAIRE", "parcellaire_express_pci": "Parcellaire_Express_PCI", "comparatif_vegetation": "COMPARATIF_VEGETATION", "jpeg2000": "JPEG2000", "metadonnees": "METADONNEES", "projets_carto": "PROJETS_CARTO"};
 
@@ -8,9 +8,10 @@ for (var d in docus) {
 }
 $('#docs').html(links);
 $('#docs a').css({"border": "1px solid #bababa", "border-radius": "4px", "font-size": "0.7em", "padding": "2px 5px", "margin": "0 5px 8px 0", "display": "inline-block"});
+$('.size').css({"float": "right", "font-variant": "super", "color": "#A0A0A0"});
 
 const documentation = $("#docs a");
-const fname = /([^\/]{1,})$/;
+const fnameRegex = /([^\/]{1,})$/;
 const altUrl = "https://data.cquest.org";
 const downloadable = $.merge($("a[href*='7z']"), $("a[href*='zip']"));
 var opendatarchive = {};
@@ -19,68 +20,107 @@ $('#values').focus();
 
 function filterFileNames(asText = false) {
   $('#results').html('');
-  $('#docs a').css('display', 'none');
-  alt = $('#alt').prop("checked");
-  arr = [];
-  var text = '';
-  var v = $('#values').val().trim().toUpperCase().split(' ');
-  var results = downloadable;
-  var docs = documentation;
-  for (var i in v) {
-    if (v[i].startsWith('-')) {
-      results = results.not("a[href*='"+v[i].substring(1)+"']")
-      $("#docs a:contains('"+v[i].substring(1)+"')").css('display', 'none');
+  var v = $('#values').val().trim().toUpperCase();
+  
+  if (v.length > 0) {
+    $('#docs a').css('display', 'none');
+    alt = $('#alt').prop("checked");
+    arr = [];
+    var text = '';
+    v = v.split(' ');
+    var results = downloadable;
+    var docs = documentation;
+    for (var i in v) {
+      if (v[i].startsWith('-')) {
+        results = results.not("a[href*='"+v[i].substring(1)+"']")
+        $("#docs a:contains('"+v[i].substring(1)+"')").css('display', 'none');
+      }
+      else {
+        results = results.filter("a[href*='"+v[i]+"']");
+        $("#docs a:contains('"+v[i]+"')").css('display', 'inline-block');
+      }
     }
-    else {
-      results = results.filter("a[href*='"+v[i]+"']");
-      $("#docs a:contains('"+v[i]+"')").css('display', 'inline-block');
-    }
-  }
-  results.each(function() {
-    arr.push($(this).attr("href"));
-  });
-  if (arr.length > 0) {
-    var r = 'résultats';
-    if (arr.length === 1) {
-      r = 'résultat';
-    }
-    if (asText) {
+    results.each(function() {
+      arr.push($(this).attr("href"));
+    });
+
+    if (arr.length > 0) {
+      var r = 'résultats';
+      if (arr.length === 1) {
+        r = 'résultat';
+      }
+
+      var size = 0;
+      var nosize = 0;
+
       for (var i in arr.sort()) {
-        if (alt) {
-          var match = arr[i].match(fname);
-          if (match[1] in opendatarchive) {
-            text += altUrl+opendatarchive[match[1]].path+match[1]+"\n";
+        var fname = arr[i].match(fnameRegex)[1];
+        var fnameOA = false;
+
+        if (fname in opendatarchive) {
+          fnameOA = fname;
+        }
+        /* More checks needed to do this :
+        else if (fname.replace('.001', '') in opendatarchive) {
+          fnameOA = fname.replace('.001', '');
+        }
+        else if (fname+'.001' in opendatarchive) {
+          fnameOA = fname+'.001';
+        }
+        */
+
+        if (fnameOA === false) {
+          nosize += 1;
+        }
+        else {
+          size += opendatarchive[fnameOA].size;
+        }
+
+        if (asText) {
+          if (alt === true && fnameOA !== false) {
+            text += altUrl+opendatarchive[fnameOA].path+fname+"\n";
           }
           else {
             text += arr[i]+"\n";
           }
         }
         else {
-          text += arr[i]+"\n";
-        }
-      }
-      $('#results').html('<p>'+arr.length+' '+r+' :</p><textarea style="width:100%;height:200px;">'+text+'</textarea>');
-    }
-    else {
-      for (var i in arr.sort()) {
-        var match = arr[i].match(fname);
-        if (alt) {
-          if (match[1] in opendatarchive) {
-            text += '<a href="'+altUrl+opendatarchive[match[1]].path+match[1]+'">'+match[1]+'</a><br />'+"\n";
+          if (alt === true && fnameOA !== false) {
+            text += '<a href="'+altUrl+opendatarchive[fnameOA].path+fnameOA+'">'+fname+'<span  class="size">'+formatBytes(opendatarchive[fnameOA].size)+'</span></a><br />'+"\n";
+          }
+          else if (fnameOA !== false) {
+            text += '<a href="'+arr[i]+'">'+fname+'<span class="size">'+formatBytes(opendatarchive[fnameOA].size)+'</span></a><br />'+"\n";
           }
           else {
-            text += '<a href="'+arr[i]+'">'+match[1]+'</a><br />'+"\n";
+            text += '<a href="'+arr[i]+'">'+fname+'</a><br />'+"\n";
           }
         }
-        else {
-          text += '<a href="'+arr[i]+'">'+match[1]+'</a><br />'+"\n";
-        }
       }
-      $('#results').html('<p>'+arr.length+' '+r+' :</p>'+"\n"+text);
+      
+      var results_size = '';
+      
+      if (size !== 0) {
+        results_size += formatBytes(size)
+      }
+      
+      if (nosize !== 0) {
+        results_size += ' + '+nosize+' fichiers de taille inconnue'
+      }
+
+      if (asText) {
+        $('#results').html('<p title="'+results_size+'">'+arr.length+' '+r+' :</p><textarea style="width:100%;height:200px;">'+text+'</textarea>');
+      }
+      else {
+        $('#results').html('<p title="'+results_size+'">'+arr.length+' '+r+' :</p>'+"\n"+text);
+  			$('.size').css({"float": "right", "font-variant": "super", "color": "#A0A0A0"});
+      }
+    }
+    else {
+      $('#results').html('<p>Aucun résultat</p>');
     }
   }
   else {
-    $('#results').html('<p>Aucun résultat</p>');
+    $('#docs a').css('display', 'inline-block');
   }
 }
 
@@ -94,6 +134,16 @@ $("#values").keypress(function(event) {
     filterFileNames();
   }
 });
+
+// from: https://stackoverflow.com/a/18650828
+function formatBytes(bytes, decimals = 2) {
+  if (bytes === 0) return '0 Bytes';
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['b', 'Ko', 'Mo', 'Go', 'To', 'Po', 'Eo', 'Zo', 'Yo'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+}
 
 $.getScript( "https://bjperson.github.io/ign-bookmarklet/resources/opendatarchives.js" )
   .done(function() {
