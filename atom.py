fbc627948a2f97c4e47b867f35a33725d06a5391
bjperson <brice@ideeslibres.org>
2021-06-15 01:44:31 +0200

    Add atom feed

---
+++ atom.py
@@ -0,0 +1,117 @@
+#! /usr/bin/env python3
+# -*- coding: utf8 -*-
+import json, math
+from datetime import datetime
+from collections import OrderedDict
+
+def formatBytes(b):
+  if b == 0:
+    return '0 b'
+  k = 1024
+  sizes = ['b', 'Ko', 'Mo', 'Go', 'To', 'Po', 'Eo', 'Zo', 'Yo']
+  i = math.floor(math.log(b) / math.log(k))
+  n = b / math.pow(k, i)
+  return  "{0:.2f}".format(n)+' '+sizes[i]
+
+with open('./resources/archives.js', 'r') as archives_file:
+  json_str = archives_file.read().replace("\n", " ")[10:-2]
+  archives_unsorted = json.loads(json_str)
+
+archives = sorted(
+  archives_unsorted,
+  key=lambda x: datetime.strptime(x['time'], "%Y-%m-%dT%H:%M"), reverse=True
+)
+
+last_update = archives[0]['time']
+archives_latest = {}
+
+for item in archives:
+  if item['name'] not in archives_latest:
+    archives_latest[item['name']] = item
+
+with open('./resources/libre.json', 'r') as ign_file:
+  ign = json.loads(ign_file.read())
+
+ign_list = []
+ign_dict = {}
+
+for item in ign:
+  ign_list.append(item['name'])
+  ign_dict[item['name']] = item
+
+rss = OrderedDict()
+
+for item in archives_latest:
+  if item in ign_list:
+    day = archives_latest[item]['time'].split('T')[0]
+    product = archives_latest[item]['name'].split('_')[0]
+    if day not in rss:
+      rss[day] = {}
+    if product not in rss[day]:
+      rss[day][product] = []
+    rss[day][product].append(archives_latest[item])
+
+xml = '''\
+<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+  <title>Open Data IGN</title>
+  <link href="https://github.com/bjperson/ign-bookmarklet"/>
+  <updated>{last}:00Z</updated>
+  <id>https://geoservices.ign.fr/documentation/diffusion/telechargement-donnees-libres.html</id>\
+'''.format(last=last_update)
+
+nb_entry = 0
+
+for day in rss:
+
+  if nb_entry < 21:
+
+    summary = []
+    html = ''
+
+    for product in rss[day]:
+
+      nb = str(len(rss[day][product]))
+      product_size = 0
+
+      plur = 's' if len(rss[day][product]) > 1 else ''
+
+      html += '''<h1>{p}</h1>
+          <p>
+          <strong>{n} fichier{s} ajouté{s} :</strong><br /><br />
+          '''.format(p=product, n=nb, s=plur)
+
+      products = sorted(rss[day][product], key=lambda x: x['name'])
+
+      for updated in products:
+        html +=  '''<a href="{p}{n}">{n}</a> {b}<br />
+          '''.format(p=ign_dict[updated['name']]['path'], n=updated['name'], b=formatBytes(updated['size']))
+        product_size += updated['size']
+
+      html += '''<br />
+          Total: {t}</p>'''.format(t=formatBytes(product_size))
+
+      summary.append(product+' ('+nb+')')
+
+    entry = '''
+    <entry>
+      <title>{p}</title>
+      <link rel="related" href="https://geoservices.ign.fr/documentation/diffusion/telechargement-donnees-libres.html"/>
+      <id>https://geoservices.ign.fr/documentation/diffusion/telechargement-donnees-libres.html#{d}</id>
+      <updated>{d}T09:00:00Z</updated>
+      <summary>{p}</summary>
+      <content type="xhtml">
+        <div xmlns="http://www.w3.org/1999/xhtml">
+          {h}
+        </div>
+      </content>
+    </entry>\
+    '''.format(d=day, h=html, p=", ".join(summary))
+
+    xml += entry;
+
+    nb_entry += 1
+
+xml += "\n</feed>"
+
+print(xml)
---
+++ ign.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+file_changed=false
+
+wget -q https://geoservices.ign.fr/documentation/diffusion/telechargement-donnees-libres.html -O ./resources/libre.html
+
+extracted=$(cat ./resources/libre.html | grep href | grep "7z\|zip\|gpkg" | sed 's|[^"]\{1,\}"\([^"]\{1,\}\)".\{1,\}|\1|' | sort | uniq | sed 's|\([^/]\{1,\}\)$| \1|' | sed 's|\([^ ]\{1,\}\) \(.\{1,\}\)|{"path":"\1","name":"\2"},|');
+json="[${extracted::-1}]";
+
+if jq -e . >/dev/null 2>&1 <<<"$json"
+then
+  echo "$json" > ./resources/libre2.json
+fi
+
+if [ -f "./resources/libre2.json" ]
+then
+  if [ -f "./resources/libre.json" ]
+  then
+    old_file=$(md5sum ./resources/libre.json | sed 's|  [^ ]\{1,\}$||');
+    new_file=$(md5sum ./resources/libre2.json | sed 's|  [^ ]\{1,\}$||');
+    if [ "$old_file" != "$new_file" ]
+    then
+      #echo "md5 mismatch";
+      old_size=$(wc -c ./resources/libre.json | sed 's| [^ ]\{1,\}$||');
+      new_size=$(wc -c ./resources/libre2.json | sed 's| [^ ]\{1,\}$||');
+      half_old_size=$(($old_size / 2));
+      if [ $new_size -gt $half_old_size ]
+      then
+        #echo "new file size greater than half of old file size";
+        rm ./resources/libre.json
+        mv ./resources/libre2.json ./resources/libre.json
+        file_changed=true
+      fi
+    fi
+  else
+    #echo "First libre.json";
+    mv ./resources/libre2.json ./resources/libre.json
+    file_changed=true
+  fi
+fi
+
+if [ "$file_changed" = true ]
+then
+  python3 atom.py > ./resources/atom.xml
+  git config user.name "github-actions (bot)"
+  git config user.email ign-bookmarklet@noreply.github.com
+  git add ./resources/libre.json
+  git add ./resources/atom.xml
+  git commit -m "Updated feed"
+  git push
+fi
+
+
